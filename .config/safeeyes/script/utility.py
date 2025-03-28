11bf95f153c1a956e72a0226fc11f197faa604a4
kayc <ghstkayc@gmail.com>
2025-03-28 15:17:18 +0545

    update: safeeyes idle

--- .config/safeeyes/script/utility.py
+++ .config/safeeyes/script/utility.py
@@ -7,3 +7,14 @@ def check_fullscreen():
     except Exception as e:
         logging.error(e)
     return False    
+
+def check_idle():
+    IDLE_FILE = '/tmp/idle_status'
+    if not os.path.isfile(IDLE_FILE):
+        return False
+    with open(IDLE_FILE, 'r') as f:
+        idle = f.read().splitlines()[0]
+    if idle == '1':
+        return True
+    return False
+

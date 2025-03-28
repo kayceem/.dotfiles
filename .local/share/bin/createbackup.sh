39963a2dd3841a90f80a80f06ca2e1e5686d2382
kayc <ghstkayc@gmail.com>
2025-03-28 15:17:04 +0545

    update: backup.sh

--- .local/share/bin/createbackup.sh
+++ .local/share/bin/createbackup.sh
@@ -5,18 +5,13 @@ REPO="/home/kayc/.backup/backup-arch-kayc"
 ARCHIVE_PREFIX="1671bc-$(date +'%Y-%m-%d_%H-%M')"
 
 EXCLUDES=(
-    --exclude '/home/kayc/.ViberPC'
-    --exclude '/home/kayc/.dotfiles'
-    --exclude '/home/kayc/.backup'
-    --exclude '/home/kayc/.vscode'
-    --exclude '/home/kayc/.vscode-root'
+    --exclude '/home/kayc/.*'
     --exclude '/home/kayc/Code'
     --exclude '/home/kayc/Misc'
     --exclude '/home/kayc/Downloads'
-    --exclude '/home/kayc/.cache'
-    --exclude '/home/kayc/.gradle'
+    --exclude '/home/kayc/School'
+    --exclude '/home/kayc/Games'
     --exclude '/home/kayc/FlatpakApps'
-    --exclude '/home/kayc/.lmstudio'
     --exclude '*/waydroid'
     --exclude '*/uv'
     --exclude '*/.docker'
